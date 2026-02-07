c029bd50df266e68a89189ab62b6c43d84eb4e97
niru0744s <nirupambhattacharya100@gmail.com>
2026-02-07 17:56:57 +0530

    updated 5

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,7 +3,7 @@ const { registerUser, loginUser } = require("../services/auth.service.js");
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
