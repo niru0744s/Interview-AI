56d15b82eff4c695e2eb8f376c76887084a5f90d
niru0744s <nirupambhattacharya100@gmail.com>
2026-02-07 17:16:21 +0530

    modified 2

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const templateRoutes = require("./routes/template.routes");
 app.use(helmet());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 }));
 
